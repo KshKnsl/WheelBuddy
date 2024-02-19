15037839959e9a0e7db1ab35f87c3c4c0661609e
aaditya <aaditwocode@gmail.com>
2024-02-19 22:41:27 +0530

    next commit

--- Login.cpp
+++ Login.cpp
@@ -1,1 +1,23 @@
 #include<iostream>
+#include <fstream> 
+#include <string>
+
+using namespace std;
+int main()
+{
+    homePage();
+    return 0;
+}
+
+void homepage(){
+    int choice;
+
+    cout << endl;
+    cout << "Main Menu: " << endl;
+    cout << "-------------------------------" << endl;
+    cout << "1. Register" << endl;
+    cout << "2. Login" << endl;
+    cout << endl;
+    cout << "Your choice: ";
+    cin >> choice; 
+}
