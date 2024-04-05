9032828252fa7c8be139f30ed780cd259004752a
Kush Knsl <mrkushkansal@gmail.com>
2024-04-05 13:57:38 +0530

    Some small fixes

--- Parser.cpp
+++
@@ -1,8 +0,0 @@
-
-int main() 
-{
-    string folderLocName = "./Files/1234"; // Replace with actual folder name
-    Parser parser;
-    parser.sortRides(folderLocName + "/upcomingRides.txt", folderLocName + "/pastRides.txt");
-    return 0;
-}
