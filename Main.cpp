f5895401f4a106aa109c4118a1aa87e8bb03b5e8
Kush Knsl <mrkushkansal@gmail.com>
2024-04-10 00:33:06 +0530

    Formatting issue in toString() Fixed

--- Main.cpp
+++ Main.cpp
@@ -138,10 +138,10 @@ public:
         ofstream pastRidesFile(folderPath + "/pastRides.txt");
         ofstream upcomingRidesFile(folderPath + "/upcomingRides.txt");
         ofstream userDetailsFile(folderPath + "/userDetails.txt");
-        pastRidesFile << "| Ride ID    | Date       | Time   | Source City | Destination City | Max Passengers | Current Passengers | Car Model    | Fare(Rs) | Distance (km) |" << endl;
-        pastRidesFile << "|------------|------------|--------|-------------|------------------|----------------|--------------------|--------------|----------|---------------|" << endl;
-        upcomingRidesFile << "| Ride ID    | Date       | Time   | Source City | Destination City | Max Passengers | Current Passengers | Car Model    | Fare(Rs) | Distance (km) |" << endl;
-        upcomingRidesFile << "|------------|------------|--------|-------------|------------------|----------------|--------------------|--------------|----------|---------------|" << endl;
+        pastRidesFile <<     "| Ride ID    | Date        | Time   | Source City  | Destination City  | Max Passengers | Current Passengers  | Car Model     | Fare(Rs) | Distance (km) |" << endl;
+        pastRidesFile <<     "|------------|-------------|--------|--------------|-------------------|----------------|---------------------|---------------|----------|---------------|" << endl;
+        upcomingRidesFile << "| Ride ID    | Date        | Time   | Source City  | Destination City  | Max Passengers | Current Passengers  | Car Model     | Fare(Rs) | Distance (km) |" << endl;
+        upcomingRidesFile << "|------------|-------------|--------|--------------|-------------------|----------------|---------------------|---------------|----------|---------------|" << endl;
         // Close the files
         pastRidesFile.close();
         upcomingRidesFile.close();
@@ -289,25 +289,25 @@ public:
              << endl;
         file.close();
         cout << "Past Rides Details:" << endl;
-        cout << "\t-----------------------------------------------------------------------------------------------------------------------------------------------------" << endl;
+        cout << "\t----------------------------------------------------------------------------------------------------------------------------------------------------------" << endl;
         filename = "Files/" + username + "/pastRides.txt";
         ifstream file2(filename);
         while (getline(file2, line))
         {
             cout << "\t" << line << endl;
         }
-        cout << "\t-----------------------------------------------------------------------------------------------------------------------------------------------------\n"
+        cout << "\t----------------------------------------------------------------------------------------------------------------------------------------------------------\n"
              << endl;
         file2.close();
         cout << "Upcoming Rides Details:" << endl;
-        cout << "\t-----------------------------------------------------------------------------------------------------------------------------------------------------" << endl;
+        cout << "\t----------------------------------------------------------------------------------------------------------------------------------------------------------" << endl;
         filename = "Files/" + username + "/upcomingRides.txt";
         ifstream file3(filename);
         while (getline(file3, line))
         {
             cout << "\t" << line << endl;
         }
-        cout << "\t-----------------------------------------------------------------------------------------------------------------------------------------------------\n"
+        cout << "\t----------------------------------------------------------------------------------------------------------------------------------------------------------\n"
              << endl;
         file3.close();
     }
@@ -419,16 +419,30 @@ public:
         }
         return id;
     }
-
-    string toString()
+    string toString() 
     {
         stringstream ss;
 
-        ss << "| " << setw(10) << left << rideID << "| " << setw(10) << left << date << "| " << setw(6) << left << time << "| "
-           << setw(11) << left << sourceCity << "| " << setw(16) << left << destinationCity << "| " << setw(14) << left << maxPassengers
-           << " | " << setw(18) << left << currentPassengers << " | " << setw(11) << left << carModel << "| " << setw(7) << left << fare
-           << "  | " << setw(13) << left << distance << " |";
+         ss << "| " << setw(11) << left << truncate(rideID, 11) << "| "
+           << setw(12) << left << truncate(date, 12) << "| "
+           << setw(7) << left << truncate(time, 7) << "| "
+           << setw(13) << left << truncate(sourceCity, 13) << "| "
+           << setw(18) << left << truncate(destinationCity, 18) << "| "
+           << setw(15) << left << maxPassengers << "| "
+           << setw(20) << left << currentPassengers << "| "
+           << setw(14) << left << truncate(carModel, 14) << "| "
+           << setw(9) << left << fare << "| "
+           << setw(14) << left << distance << "|";
+
         return ss.str();
+    }
+    // Helper function to truncate strings longer than a specified length
+    string truncate(string str, size_t width) 
+    {
+        if (str.length() > width)
+            return str.substr(0, width); // Truncate the string
+        else
+            return str; // Return the original string if its length is within the width
     }
 };
 
@@ -511,9 +525,9 @@ public:
     void writeRides(string fileName, vector<Ride> &rides)
     {
         ofstream file(fileName);
-        file << "| Ride ID    | Date       | Time   | Source City | Destination City | Max Passengers | Current Passengers | Car Model    | Fare(Rs) | Distance (km) |" << endl;
-        file << "|------------|------------|--------|-------------|------------------|----------------|--------------------|--------------|----------|---------------|" << endl;
-
+        file <<     "| Ride ID    | Date        | Time   | Source City  | Destination City  | Max Passengers | Current Passengers  | Car Model     | Fare(Rs) | Distance (km) |" << endl;
+        file <<     "|------------|-------------|--------|--------------|-------------------|----------------|---------------------|---------------|----------|---------------|" << endl;
+        
         for (Ride &ride : rides)
         {
             file << ride.toString() << endl;
@@ -543,7 +557,10 @@ public:
                 return stoi(distanceLookup[i][2]);
             }
         }
-        return 0;
+        int dis;
+        cout<<"Enter the distance between source and destination : ";
+        cin>>dis;
+        return dis;
     }
 
     // Function to calculate the total cost based on distance, car capacity, and fixed costs
@@ -594,7 +611,7 @@ public:
     }
 };
 
-class Menu : public Pages
+class Menu : public Pages , public BillCalculator
 {
 public:
     // Display the menu options
@@ -689,12 +706,10 @@ private:
     {
         cout << "Booking a ride..." << endl;
         // Implementing logic for booking a ride
-        string rideID, date, time, sourceCity, destinationCity, carModel;
+        string date, time, sourceCity, destinationCity, carModel;
         int maxPassengers, currentPassengers;
         double fare, distance;
 
-        cout << "Enter Ride ID: ";
-        cin >> rideID;
         cout << "Enter Date (YYYY-MM-DD): ";
         cin >> date;
         cout << "Enter Time (HH:MM): ";
@@ -709,10 +724,8 @@ private:
         cin >> currentPassengers;
         cout << "Enter Car Model: ";
         cin >> carModel;
-        cout << "Enter Fare (Rs): ";
-        cin >> fare;
-        cout << "Enter Distance (km): ";
-        cin >> distance;
+        distance = calculateDistance(sourceCity, destinationCity);
+        fare = calculateTotalCost(distance, maxPassengers);
 
         Ride r(date, time, sourceCity, destinationCity, maxPassengers, currentPassengers, carModel, fare, distance);
         string s = r.toString();
@@ -1114,7 +1127,7 @@ int main()
         // Now display the dashboard
         Dashboard dash;
         dash.display(user->getUsername());
-        Sleep(5000);
+        Sleep(3000);
 
         Menu menu;
         // Display the menu
@@ -1127,8 +1140,8 @@ int main()
     //  calcob.printBill("Mumbai","Pune",distance,5);
 
     // Demo to use the ride class functions
-    //  Ride ride("| A1B2C      | 2023-12-20 | 08:00  | Mumbai      | Delhi            | 4              | 3                  | Toyota Camry | 1200     | 1200          |");
-    //  cout << "Ride Details: " << ride.toString() << endl;
+    // Ride ride("| A1B2C      | 2023-12-20 | 08:00  | Mumbai      | Delhi            | 4              | 3                  | Toyota Camry | 1200     | 1200          |");
+    // cout << "Ride Details: " << ride.toString() << endl;
     page.thankYouPage();
     return 0;
 }
@@ -1246,74 +1259,3 @@ int main()
 //     }
 
 
-// class BillCalculator
-// {
-//     const double FUEL_COST_PER_KM = 12.0;
-//     const double MAINTENANCE_COST_PER_KM = 0.8;
-//     const double TOLL_COST_PER_KM = 2.0;
-//     // Simple distance lookup table for preset supported Indian cities
-
-//     string distanceLookup[110][3] =
-//         {
-//             {"Mumbai", "Delhi", "1200"}, {"Mumbai", "Bangalore", "980"}, {"Mumbai", "Kolkata", "1960"}, {"Mumbai", "Chennai", "1340"}, {"Mumbai", "Hyderabad", "620"}, {"Mumbai", "Pune", "150"}, {"Mumbai", "Jaipur", "1200"}, {"Mumbai", "Ahmedabad", "530"}, {"Mumbai", "Lucknow", "1500"}, {"Mumbai", "Bhopal", "770"}, {"Delhi", "Bangalore", "1800"}, {"Delhi", "Kolkata", "1470"}, {"Delhi", "Chennai", "2180"}, {"Delhi", "Mumbai", "1200"}, {"Delhi", "Hyderabad", "1500"}, {"Delhi", "Pune", "1550"}, {"Delhi", "Jaipur", "280"}, {"Delhi", "Ahmedabad", "940"}, {"Delhi", "Lucknow", "440"}, {"Delhi", "Bhopal", "780"}, {"Bangalore", "Mumbai", "980"}, {"Bangalore", "Delhi", "1800"}, {"Bangalore", "Kolkata", "2000"}, {"Bangalore", "Chennai", "350"}, {"Bangalore", "Hyderabad", "560"}, {"Bangalore", "Pune", "840"}, {"Bangalore", "Jaipur", "1700"}, {"Bangalore", "Ahmedabad", "1200"}, {"Bangalore", "Lucknow", "2000"}, {"Bangalore", "Bhopal", "1230"}, {"Kolkata", "Mumbai", "1960"}, {"Kolkata", "Delhi", "1470"}, {"Kolkata", "Bangalore", "2000"}, {"Kolkata", "Chennai", "1600"}, {"Kolkata", "Hyderabad", "1450"}, {"Kolkata", "Pune", "1800"}, {"Kolkata", "Jaipur", "1600"}, {"Kolkata", "Ahmedabad", "1850"}, {"Kolkata", "Lucknow", "1050"}, {"Kolkata", "Bhopal", "1450"}, {"Chennai", "Mumbai", "1340"}, {"Chennai", "Delhi", "2180"}, {"Chennai", "Bangalore", "350"}, {"Chennai", "Kolkata", "1600"}, {"Chennai", "Hyderabad", "680"}, {"Chennai", "Pune", "1200"}, {"Chennai", "Jaipur", "2300"}, {"Chennai", "Ahmedabad", "1700"}, {"Chennai", "Lucknow", "2000"}, {"Chennai", "Bhopal", "1650"}, {"Hyderabad", "Mumbai", "620"}, {"Hyderabad", "Delhi", "1500"}, {"Hyderabad", "Bangalore", "560"}, {"Hyderabad", "Kolkata", "1450"}, {"Hyderabad", "Chennai", "680"}, {"Hyderabad", "Pune", "620"}, {"Hyderabad", "Jaipur", "1100"}, {"Hyderabad", "Ahmedabad", "970"}, {"Hyderabad", "Lucknow", "1600"}, {"Hyderabad", "Bhopal", "1020"}, {"Pune", "Mumbai", "150"}, {"Pune", "Delhi", "1550"}, {"Pune", "Bangalore", "840"}, {"Pune", "Kolkata", "1800"}, {"Pune", "Chennai", "1200"}, {"Pune", "Hyderabad", "620"}, {"Pune", "Jaipur", "1300"}, {"Pune", "Ahmedabad", "650"}, {"Pune", "Lucknow", "1700"}, {"Pune", "Bhopal", "1030"}, {"Jaipur", "Mumbai", "1200"}, {"Jaipur", "Delhi", "280"}, {"Jaipur", "Bangalore", "1700"}, {"Jaipur", "Kolkata", "1600"}, {"Jaipur", "Chennai", "2300"}, {"Jaipur", "Hyderabad", "1100"}, {"Jaipur", "Pune", "1300"}, {"Jaipur", "Ahmedabad", "660"}, {"Jaipur", "Lucknow", "550"}, {"Ahmedabad", "Mumbai", "530"}, {"Ahmedabad", "Delhi", "940"}, {"Ahmedabad", "Bangalore", "1200"}, {"Ahmedabad", "Kolkata", "1850"}, {"Ahmedabad", "Chennai", "1700"}, {"Ahmedabad", "Hyderabad", "970"}, {"Ahmedabad", "Pune", "650"}, {"Ahmedabad", "Jaipur", "660"}, {"Ahmedabad", "Lucknow", "1350"}, {"Ahmedabad", "Bhopal", "580"}, {"Lucknow", "Mumbai", "1500"}, {"Lucknow", "Delhi", "440"}, {"Lucknow", "Kolkata", "1050"}, {"Lucknow", "Chennai", "2000"}, {"Lucknow", "Hyderabad", "1600"}, {"Lucknow", "Pune", "1700"}, {"Lucknow", "Jaipur", "550"}, {"Lucknow", "Ahmedabad", "1350"}, {"Lucknow", "Bhopal", "650"}, {"Bhopal", "Mumbai", "770"}, {"Bhopal", "Delhi", "780"}, {"Bhopal", "Bangalore", "1230"}, {"Bhopal", "Kolkata", "1450"}, {"Bhopal", "Chennai", "1650"}, {"Bhopal", "Hyderabad", "1020"}, {"Bhopal", "Pune", "1030"}, {"Bhopal", "Jaipur", "580"}, {"Bhopal", "Ahmedabad", "650"}, {"Bhopal", "Lucknow", "650"}};
-
-// public:
-//     int calculateDistance(string source, string destination)
-//     {
-//         for (int i = 0; i < 110; i++)
-//         {
-//             if (distanceLookup[i][0] == source && distanceLookup[i][1] == destination)
-//             {
-//                 return stoi(distanceLookup[i][2]);
-//             }
-//         }
-//         return 0;
-//     }
-
-//     // Function to calculate the total cost based on distance, car capacity, and fixed costs
-//     double calculateTotalCost(int distance, int carCapacity)
-//     {
-//         double fuelCost = distance * FUEL_COST_PER_KM;
-//         double maintenanceCost = distance * MAINTENANCE_COST_PER_KM;
-//         double tollCost = distance * TOLL_COST_PER_KM;
-//         double additionalCost = carCapacity * 20.0; // Adjust this value as needed
-//         double totalCost = fuelCost + maintenanceCost + tollCost + additionalCost;
-//         return totalCost;
-//     }
-
-//     // Function to print the bill in a proper format
-//     void printBill(string source, string destination, int distance, int carCapacity)
-//     {
-//         system("cls");
-//         system("color A1");
-//         double totalCost = calculateTotalCost(distance, carCapacity);
-//         cout << "\n";
-//         cout << "\n";
-//         cout << "\t\t\t\t\t\t" << "\n";
-
-//         cout << "\t\t\t\t\t\t" << setw(60) << "|************************* Trip Bill **************************|\n";
-//         cout << "\t\t\t\t\t\t" << "|                                                              |" << endl;
-//         cout << "\t\t\t\t\t\t" << "|\tSource City: \t\t" << setw(25) << left << source << "      |" << endl;
-//         cout << "\t\t\t\t\t\t" << "|\tDestination City: \t" << setw(25) << left << destination << "      |" << endl;
-//         cout << "\t\t\t\t\t\t" << "|\tDistance: \t\t" << setw(15) << left << distance << " km             |\n";
-//         cout << "\t\t\t\t\t\t" << "|\tCar Capacity: \t\t" << setw(15) << left << carCapacity << " passengers     |\n";
-//         cout << "\t\t\t\t\t\t" << "|\t-------------------------------------------------------|\n";
-//         cout << "\t\t\t\t\t\t" << "|\tFuel Cost: \t\t\tRs." << setw(20) << left << distance * FUEL_COST_PER_KM << "|" << endl;
-//         cout << "\t\t\t\t\t\t" << "|\tMaintenance Cost: \t\tRs." << setw(20) << left << distance * MAINTENANCE_COST_PER_KM << "|" << endl;
-//         cout << "\t\t\t\t\t\t" << "|\tToll Cost: \t\t\tRs." << setw(20) << left << distance * TOLL_COST_PER_KM << "|" << endl;
-//         cout << "\t\t\t\t\t\t" << "|\tAdditional Cost: \t\tRs." << setw(20) << left << carCapacity * 20.0 << "|" << endl;
-//         cout << "\t\t\t\t\t\t" << "|\tDriver Cost: \t\t\tRs." << setw(20) << left << 200 << "|" << endl;
-//         cout << "\t\t\t\t\t\t" << "|\t-------------------------------------------------------|\n";
-//         cout << "\t\t\t\t\t\t" << "|\tTotal Cost: \t\t\tRs." << setw(10) << left << totalCost + 200 << "  only    |" << endl;
-//         cout << "\t\t\t\t\t\t" << "|                                                              |" << endl;
-//         cout << "\t\t\t\t\t\t" << setw(60) << "|   # This is a computer-generated invoice and it does not     |" << endl;
-//         cout << "\t\t\t\t\t\t" << setw(60) << "|     require an authorized signature #                        |" << endl;
-//         cout << "\t\t\t\t\t\t" << "|                                                              |" << endl;
-//         cout << "\t\t\t\t\t\t" << setw(60) << "|//////////////////////////////////////////////////////////////|" << endl;
-//         cout << "\t\t\t\t\t\t" << setw(60) << "|You are advised to pay up the amount before the due date.     |" << endl;
-//         cout << "\t\t\t\t\t\t" << setw(60) << "|\t\tOtherwise, a penalty fee will be applied       |" << endl;
-//         cout << "\t\t\t\t\t\t" << setw(60) << "|//////////////////////////////////////////////////////////////|" << endl;
-//         cout << "\t\t\t\t\t\t" << "|                                                              |" << endl;
-//         cout << "\t\t\t\t\t\t" << setw(60) << "************************** Thank You! **************************\n";
-//     }
-// };
